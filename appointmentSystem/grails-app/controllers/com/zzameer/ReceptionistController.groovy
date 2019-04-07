aafdac07fab9f8cb7e20f3169f8d15aa894924d2
student <student@b6026504.localdomain>
2019-04-07 17:26:50 +0100

    Added login functionality to receptionist controller

--- appointmentSystem/grails-app/controllers/com/zzameer/ReceptionistController.groovy
+++ appointmentSystem/grails-app/controllers/com/zzameer/ReceptionistController.groovy
@@ -3,4 +3,39 @@ package com.zzameer
 class ReceptionistController {
 
     def scaffold = Receptionist
+
+def login(){
+
+}
+
+def validate(){
+
+def user = Receptionist.findByrecepUsername(params.username)
+
+if(user && user.recepPassword == paramas.password){
+
+session.user=user
+
+render view:'home'
+
+}
+
+else{
+
+flash.message = "invalid username and password."
+
+render view:'login'
+
+}
+
+}
+
+def logout={
+
+session.user = null
+
+redirect(uri:'/)
+
+}
+
 }
